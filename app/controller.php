d320407d2c578a836bb2f2556b4340030b62b576
Matias De lellis <mati86dl@gmail.com>
2014-07-05 20:14:03 -0300

    Rename views files, to respect folders structure..

--- app/controller.php
+++ app/controller.php
@@ -3,29 +3,29 @@ class MyaController {
 	function show_posts ()
 	{
 		$posts = mya_connection_get_posts ();
-		require 'app/views/postsView.php';
+		require 'app/view/posts.php';
 	}
 
 	function show_post_by_slug ($slug)
 	{
 		$post = mya_connection_get_post_by_slug ($slug);
-		require 'app/views/postView.php';
+		require 'app/view/post.php';
 	}
 
 	function compose_new_post ()
 	{
 		if (Auth::instance()->is_logged())
-			require 'app/views/composerView.php';
+			require 'app/view/composer.php';
 	}
 
 	function login ()
 	{
 		if (!Auth::instance()->is_logged()) {
-			require 'app/views/loginView.php';
+			require 'app/view/login.php';
 		}
 		else {
 			$posts = mya_connection_get_posts ();
-			require 'app/views/postsView.php';
+			require 'app/view/posts.php';
 		}
 	}
 
@@ -36,18 +36,18 @@ class MyaController {
 		}
 
 		$posts = mya_connection_get_posts ();
-		require 'app/views/postsView.php';
+		require 'app/view/posts.php';
 	}
 
 	function show_admin_page ()
 	{
 		if (Auth::instance()->is_logged()) {
 			$users = mya_connection_get_users ();
-			require 'app/views/adminView.php';
+			require 'app/view/admin.php';
 		}
 		else {
 			$posts = mya_connection_get_posts ();
-			require 'app/views/postsView.php';
+			require 'app/view/posts.php';
 		}
 	}
 }
