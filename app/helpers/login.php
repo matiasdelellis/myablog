97c497affc25443a739f8545d4529c28f7670b53
Matias De lellis <mati86dl@gmail.com>
2014-07-12 12:00:00 -0300

    Use DOCUMENT_ROOT to fix includes on form actions scripts.

--- app/helpers/login.php
+++ app/helpers/login.php
@@ -1,8 +1,7 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . '/var/www/html/myablog');
-
-require_once 'app/core/auth.php';
-require_once 'app/core/database.php';
+$HOST_ROOT = $_SERVER["DOCUMENT_ROOT"];
+require_once ($HOST_ROOT.'/app/core/auth.php');
+require_once ($HOST_ROOT.'/app/core/database.php');
 
 $username = $_POST['Username'];
 $password = $_POST['Password'];
--- app/helpers/uploadPost.php
+++ app/helpers/uploadPost.php
@@ -1,6 +1,6 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . '/var/www/html/myablog');
-require_once 'app/model/post.php';
+$HOST_ROOT = $_SERVER["DOCUMENT_ROOT"];
+require_once ($HOST_ROOT.'/app/model/post.php');
 
 $title = $_POST['inputTitle'];
 $text = $_POST['inputText'];
